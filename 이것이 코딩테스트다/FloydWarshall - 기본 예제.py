ae53834060f212e602a56d027f0757faa4880606
JeongDongYoon1002 <65494409+JeongDongYoon1002@users.noreply.github.com>
2021-11-04 14:22:31 +0900

    FloydWarshall - 기본 예제.py
    FloydWarshall - 기본 예제.py

---
+++ 이것이 코딩테스트다/FloydWarshall - 기본 예제.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline 
+INF = int(1e9)
+
+n = int(input()) #노드의 개수
+m = int(input()) # 간선의 갯수
+
+graph = [[INF]*(n+1) for i in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if i == j:
+            graph[i][j] = 0
+
+for i in range(m):
+    a, b, c = map(int,input().split())
+    graph[a][b] = c 
+
+
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1,n+1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if graph[i][j] == INF :
+            print("INFINITY", end = " ")
+        else :
+            print(graph[i][j], end = " ")
+
+    print()
+
+
+
+    
