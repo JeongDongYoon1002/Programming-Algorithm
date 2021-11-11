5548e6895fce8a51de637ea4d6ac78dfa4d43bad
JeongDongYoon1002 <65494409+JeongDongYoon1002@users.noreply.github.com>
2021-11-11 15:22:04 +0900

    BFS&DFS -  괄호 변환.py
    BFS&DFS -  괄호 변환.py

---
+++ 이것이 코딩테스트다/BFS&DFS -  괄호 변환.py
@@ -0,0 +1,50 @@
+# '(' ')' 개수가 같다면 균형잡힌 괄호 문자열
+# '(' ')' 짝도 모두 맞을경우 올바른 괄호문자열 
+def balanced_index(p):
+    count = 0
+    for i in range(len(p)):
+        if p[i] == '(':
+            count += 1
+        else :
+            count -= 1
+        if count == 0:
+            return i
+        
+def check_proper(arr):
+    count = 0 # '(' 개수
+    for i in arr:
+        if i == '(':
+            count += 1
+        else:
+            if count == 0:
+                return False 
+            count -= 1
+    return True
+        
+def solution(p):
+    answer = ''
+    if p == '':
+        return answer
+    
+    index = balanced_index(p)
+    u = p[:index+1]
+    v = p[index+1 :]
+    
+    if check_proper(u):
+        answer = u + solution(v)
+    else:
+        answer = '('
+        answer += solution(v)
+        answer += ')'
+        u = list(u[1:-1])
+        for i in range(len(u)):
+            if u[i] == '(':
+                u[i] = ')'
+            else:
+                u[i] = '('
+                
+        answer += "".join(u)
+        
+    
+    return answer
+  
