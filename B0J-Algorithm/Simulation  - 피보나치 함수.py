11bfac289db2295a03273be2d42937dfe996d0b2
JeongDongYoon1002 <65494409+JeongDongYoon1002@users.noreply.github.com>
2021-11-17 21:45:03 +0900

    Simulation  - 피보나치 함수.py
    Simulation  - 피보나치 함수.py

---
+++ B0J-Algorithm/Simulation  - 피보나치 함수.py
@@ -0,0 +1,17 @@
+def fibo(n):
+    length = len(zero)
+    if n <= 2:
+        print(zero[n], one[n])
+    else:
+        for i in range(length, n+1):
+            zero.append(zero[i-1]+zero[i-2])
+            one.append(one[i-1]+one[i-2])
+
+        print(zero[n], one[n])
+
+a = int(input()) 
+zero = [1, 0, 1]
+one = [0, 1, 1]
+
+for i in range(a):
+    fibo(int(input()))
