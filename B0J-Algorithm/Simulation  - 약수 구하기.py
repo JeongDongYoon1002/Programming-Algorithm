13dcd62a68eaea951e28e4d8b0bc546d2835b989
JeongDongYoon1002 <65494409+JeongDongYoon1002@users.noreply.github.com>
2021-11-18 12:42:03 +0900

    Simulation  - 약수 구하기.py
    Simulation  - 약수 구하기.py

---
+++ B0J-Algorithm/Simulation  - 약수 구하기.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline 
+
+n, k = map(int,input().split())
+arr = []
+
+for i in range(1, n+1):
+    if n % i == 0:
+        arr.append(i)
+        
+if len(arr) >= k:
+    print(arr[k-1])
+else:
+    print(0)
